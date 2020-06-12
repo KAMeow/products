c39183f343aa87082431a1298d6a0d8c7c9b40d6
Katrina <katrinayu89@gmail.com>
2020-06-12 15:17:31 +0800

    update

--- products.py
+++ products.py
@@ -8,3 +8,15 @@ while True:
 	products.append([name, price])
 print(products)
 
+
+for p in products:
+	print(p[0], 'price is', p[1])
+
+#字串只可以字串和字串做合并，不能做减除
+# 'abc' + '123' = 'abc123'
+# 'abc' * '123' = 'abc'
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品, 价格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
